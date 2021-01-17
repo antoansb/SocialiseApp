c7d32c9ddc24a6b3d0c704c7c8a60e7737e38fb0
Lazar Antonijevic <lakyspider@gmail.com>
2021-01-17 02:06:47 +0100

    Register/Login starting point

--- app/src/main/java/com/example/socialiseapp/MainActivity.java
+++ app/src/main/java/com/example/socialiseapp/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.socialiseapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailEditText = findViewById(R.id.editTextEmail);
+        passwordEditText = findViewById(R.id.editTextPassword);
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser() != null) {
+            login();
+        }
+    }
+
+
+    public void register(View view) {
+        mAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        login();
+                    } else {
+                        mAuth.createUserWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                                .addOnCompleteListener(this, task1 -> {
+                                    if (task1.isSuccessful()) {
+                                        login();
+                                    } else {
+                                        Toast.makeText(this, "Login failed. Try again!", Toast.LENGTH_LONG).show();
+                                    }
+                                } );
+                    }
+                });
+    }
+
+    private void login() {
+        Intent intent = new Intent(this, FeedActivity.class);
+        startActivity(intent);
     }
 }
