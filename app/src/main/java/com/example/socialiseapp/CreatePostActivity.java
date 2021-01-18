1acdc1565bdabb08a54803a31a229d2899c60343
Lazar Antonijevic <lakyspider@gmail.com>
2021-01-18 03:55:57 +0100

    Creating menu, adding image & uploading it to Firebase

---
+++ app/src/main/java/com/example/socialiseapp/CreatePostActivity.java
@@ -0,0 +1,104 @@
+package com.example.socialiseapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.util.UUID;
+
+public class CreatePostActivity extends AppCompatActivity {
+
+    ImageView imageViewCreatePost = null;
+    EditText editTextMessage = null;
+    String imageName = UUID.randomUUID().toString() + ".jpg";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_post);
+
+        imageViewCreatePost = findViewById(R.id.imageViewCreatePost);
+        editTextMessage = findViewById(R.id.editTextMessage);
+    }
+
+    public void getPhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void chooseImageClick(View view) {
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        } else {
+            getPhoto();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Uri selectedImage = data.getData();
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                imageViewCreatePost.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getPhoto();
+            }
+        }
+    }
+
+    public void nextClick(View view) {
+        imageViewCreatePost.setDrawingCacheEnabled(true);
+        imageViewCreatePost.buildDrawingCache();
+        Bitmap bitmap = ((BitmapDrawable) imageViewCreatePost.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+
+        UploadTask uploadTask = FirebaseStorage.getInstance().getReference().child("images").child(imageName).putBytes(data);
+        uploadTask.addOnFailureListener(exception -> {
+            Toast.makeText(this, "Upload failed!", Toast.LENGTH_LONG).show();
+        }).addOnSuccessListener(taskSnapshot -> {
+            Log.i("Uri", taskSnapshot.getUploadSessionUri().toString());
+        });
+    }
+}
--- app/src/main/java/com/example/socialiseapp/FeedActivity.java
+++ app/src/main/java/com/example/socialiseapp/FeedActivity.java
@@ -1,14 +1,55 @@
 package com.example.socialiseapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.zip.Inflater;
 
 public class FeedActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.feed, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.createPost) {
+            Intent intent = new Intent(this, CreatePostActivity.class);
+            startActivity(intent);
+        } else if (item.getItemId() == R.id.logout) {
+            mAuth.signOut();
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        mAuth.signOut();
     }
 }
